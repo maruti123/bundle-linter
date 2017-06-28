7bd0c0ebfa1194e1ea1037f4b6b1c5b1171e95d6
maruti123 <maruthi@apigee.com>
2017-06-28 13:28:11 -0500

    Revert "issue #17"

--- test/testBundleStructure.js
+++ test/testBundleStructure.js
@@ -1,33 +1,28 @@
 //testBundleStructure.js
 
 //call the plugin against an arbitrary folder
-var myUtil = require("../lib/package/myUtil.js"),
-  bs = require("../lib/package/plugins/bundleStructure.js");
-
+var myUtil = require("../myUtil.js"),
+    bs = require("../plugins/bundleStructure.js");
 test = function(root) {
-  var results = { root, warnings: [] };
+	var results = {root,warnings:[]};
 
-  bs.onBundle({
-    root,
-    proxyRoot: root + "/" + "apiproxy",
-    warn(warning) {
-      results.warnings.push(warning);
-    }
-  });
-  myUtil.inspect(results);
+    bs.onBundle({
+        root,
+        proxyRoot: "apiproxy",
+        warn(warning) { results.warnings.push(warning); }
+    });
+    myUtil.inspect(results);
 };
 
 //process.chdir("/Users/davidallen/Projects/");
-var rootDir = "/Users/davidwallen/Projects/samples/";
-process.chdir(rootDir);
+process.chdir("/Users/davidallen/Projects/samples/");
 
 var FindFolder = require("node-find-folder"),
-  folders = new FindFolder("apiproxy/proxies");
+    folders = new FindFolder("apiproxy/proxies");
 
 folders.some(function(folder) {
-  if (folder.indexOf("target/") === -1) {
-    //drop the apiproxy off the end
-    console.log(folder);
-    test(rootDir + folder.substring(0, folder.length - 17));
-  }
+    if (folder.indexOf("target/") === -1) {
+        //drop the apiproxy off the end
+        test(folder.substring(0, folder.length - 17));
+    }
 });
